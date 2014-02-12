946e6657d9f60db607b1292d338287f1dee4f424
dycforever <dycforever@126.com>
2014-02-12 12:09:44 +0800

    update init_ubuntu.sh

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -11,6 +11,16 @@
 #cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer
 
 #apt-get install tmux
+#sudo apt-get install openssh-server
 
-#for mp3/media player
-sudo apt-get install ubuntu-restricted-extras
+# for mp3/media player
+#sudo apt-get install ubuntu-restricted-extras
+#sudo apt-get install chmsee
+
+# for python-qt
+#sudo apt-get install qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer qtcreator
+#sudo apt-get install libphonon4 libqt4-webkit
+
+# then download lucid-version libqt4-webkit from http://packages.ubuntu.com/lucid/amd64/libqt4-webkit/download
+# extract .deb and place libqt4-webkit_4.6.2-0ubuntu5.6_amd64/usr/lib/qt4/plugins/designer/libqwebview.so in /usr/share/qt4/plugins/designer
+sudo apt-get install pyqt4-dev-tools
