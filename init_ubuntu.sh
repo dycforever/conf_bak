d407bf80bd76a7abba4c6ad8806cc07b9f5a119c
ubuntu12.04 <dyc@ubuntu.(none)>
2014-02-20 00:17:08 -0800

    add cscope in init_ubuntu.sh

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sudo apt-get install vim
-#sudo apt-get install ctags
+#sudo apt-get install ctags cscope
 #sudo apt-get install git
 #sudo apt-get install cmake
 #sudo apt-get install g++
@@ -31,4 +31,4 @@
 
 # for haskell
 #
-sudo apt-get install ghc 
+#sudo apt-get install ghc 
