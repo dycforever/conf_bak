54358574457ddc6386f0cc39d8adf92298600480
dycforever <dycforever@126.com>
2014-03-02 14:29:46 +0800

    add

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-#sudo apt-get install vim
-#sudo apt-get install ctags cscope
-#sudo apt-get install git
-#sudo apt-get install cmake
-#sudo apt-get install g++
-#sudo apt-get install  python-dev
+APTGET_OPTIONS="-y"
+#APTGET_OPTIONS=""
+
+INSTALL_CMD="apt-get install $APTGET_OPTIONS"
+
+#sudo $INSTALL_CMD vim
+#sudo $INSTALL_CMD ctags cscope
+#sudo $INSTALL_CMD git
+#sudo $INSTALL_CMD cmake
+#sudo $INSTALL_CMD g++
+#sudo $INSTALL_CMD  python-dev
 #git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 #  vim and :BundleInstall or:
 #vim +BundleInstall +qall
 #cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer
 
-#sudo apt-get install tmux
-#sudo apt-get install openssh-server
+#sudo $INSTALL_CMD tmux
+#sudo $INSTALL_CMD openssh-server
 
 # for mp3/media player
 #
-#sudo apt-get install ubuntu-restricted-extras
-#sudo apt-get install chmsee
+#sudo $INSTALL_CMD ubuntu-restricted-extras
+#sudo $INSTALL_CMD chmsee
 
 # for python-qt
 # 
-#sudo apt-get install qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer qtcreator
-#sudo apt-get install libphonon4 libqt4-webkit
+#sudo $INSTALL_CMD qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer qtcreator
+#sudo $INSTALL_CMD libphonon4 libqt4-webkit
 
 # then download lucid-version libqt4-webkit from http://packages.ubuntu.com/lucid/amd64/libqt4-webkit/download
 # extract .deb and place libqt4-webkit_4.6.2-0ubuntu5.6_amd64/usr/lib/qt4/plugins/designer/libqwebview.so in /usr/share/qt4/plugins/designer
 #
-#sudo apt-get install pyqt4-dev-tools
+#sudo $INSTALL_CMD pyqt4-dev-tools
 
 # for haskell
 #
-#sudo apt-get install ghc 
+#sudo $INSTALL_CMD ghc 
+
+
+# for database 
+#
+#sudo $INSTALL_CMD mysql-server
+#
+#sudo $INSTALL_CMD --force-yes sqlite3
+#sudo $INSTALL_CMD --force-yes sqlite3
+#sudo $INSTALL_CMD --force-yes libsqlite3-dev
+
+# for php-mysql extention
+#
+#apt-get install php5-gd 
+#apt-get install php5-mysql
