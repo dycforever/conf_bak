3928db3c9de27c09449c42ac7078f7f922748969
dyc <dyc@localhost.localdomain>
2014-06-15 20:24:41 +0800

    modify ngx/configure.sh

--- nginx/configure.sh
+++ nginx/configure.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
-
+make clean
 ./configure --prefix=/home/dyc/tools/nginx \
             --with-pcre=/home/dyc/tools/pcre/ \
             --with-zlib=/home/dyc/tools/zlib \
             --with-debug  \
             --add-module=/home/dyc/tools/ngx_devel_kit \
-            --add-module=/home/dyc/tools/lua-nginx-module \
-            --add-module=/home/dyc/packages/nginx-1.4.7/src/http/modules/first_filter
+            --add-module=/home/dyc/tools/lua-nginx-module
+
+make -j4
+make install
 
